e90d3bfacd14c6a8478ec4fb2d22e4ff4b01dc92
thauanb <thauanb@gmail.com>
2024-02-17 20:24:51 -0300

    feat: melhorado a visualização dos cards dos filmes

--- src/components/BestMovies.tsx
+++ src/components/BestMovies.tsx
@@ -38,7 +38,7 @@ export default function BestMovies(){
   return(  
     <>
       <h1 className="text-4xl font-bold p-5 text-center">Melhores Filmes</h1>
-      <div className="p-5  flex flex- flex-wrap gap-5 ">
+      <div className="p-5  flex justify-evenly flex-wrap gap-7">
       
         { 
         // verifica se topMovies é vazio ou nao
@@ -48,9 +48,9 @@ export default function BestMovies(){
             topMovies.map((movie)=>{
               return( 
                 <>
-                  <div className="prose flex w-[450px] h-[600px] flex-col justify-around rounded-lg bg-gray-100 p-2">
+                  <div className="prose flex w-[450px] h-[600px] flex-col  items-center  rounded-lg  border-[6px] border-zinc-200  p-5">
                     <h3 className=" text-xl font-bold text-wrap">{movie.title}</h3>
-                    <img className="w-[400px] h-[250px]" src={`${imageBase}${movie.backdrop_path}`} alt="" />
+                    <img className="w-auto h-[250px] rounded" src={`${imageBase}${movie.backdrop_path}`} alt="" />
                     <p className="w-[400px] h-[150px] text-left ">{movie.overview}</p>
                     <MovieStatus rating={movie.vote_average} release={movie.release_date} /> 
 
