a532a404159553969e168d3c0631a34ae2bd011d
thauanb <thauanb@gmail.com>
2024-02-17 20:23:16 -0300

    refactor: removido alguns componentes não utilizados

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Home from "../pages/Home"
-import App from "../src/App"
 import Movie from "../pages/Movie"
 import './globals.css'
 
@@ -9,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Home/>} ></Route>
-        <Route path="search" element={<App/>} ></Route>
         <Route path="movies" element={<Movie/>} ></Route>
 
     </Routes>
